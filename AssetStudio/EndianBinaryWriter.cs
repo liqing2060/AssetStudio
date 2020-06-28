7c99b8b86565818301b8b6f48a4cb6351f4552a3
liqing <liqing2060@126.com>
2020-06-28 20:40:20 +0800

    EndianBinaryWriter

---
+++ AssetStudio/EndianBinaryWriter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+
+namespace AssetStudio
+{
+    public class EndianBinaryWriter : BinaryWriter
+    {
+        public EndianType endian;
+
+        public EndianBinaryWriter(Stream stream, EndianType endian = EndianType.BigEndian) : base(stream)
+        {
+            this.endian = endian;
+        }
+
+        public long Position
+        {
+            get => BaseStream.Position;
+            set => BaseStream.Position = value;
+        }
+
+        public override void Write(short value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(int value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public void Write(int value, EndianType endian)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(long value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(ushort value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(uint value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(ulong value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(float value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+
+        public override void Write(double value)
+        {
+            var buff = BitConverter.GetBytes(value);
+            if (endian == EndianType.BigEndian)
+            {
+                Array.Reverse(buff);
+            }
+            Write(buff);
+        }
+    }
+}
